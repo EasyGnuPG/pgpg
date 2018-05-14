df9fb9b3496a740623d6fd67803e33b935089c64
vedant nevetia <nevetia.vedant@gmail.com>
2018-05-15 00:46:30 +0530

    complete basic init functionality. TODO: argparser

--- src/cmd/init.py
+++ src/cmd/init.py
@@ -15,34 +15,83 @@ def cmd_init(path=None):
     if path:
         if os.path.isdir(path):
             yesno = input("There is an old directory " + path + ". Do you want to erase it? [Y/N]: ")
-            if yesno.lower() is "y":
+            if yesno.lower() == "y":
                 shutil.rmtree(path)
-                os.makedirs(path)
             else:
                 exit(1)
-        else:
-            os.makedirs(path)
     else:
         basePath = os.environ.get("HOME")
         if basePath is None:
             print("Could not find environment variable $HOME. Please try again")
             exit(1)
-        fullPath = basePath + "/.egpg"
-        if os.path.exists(fullPath):
-            yesno = input("There is an old directory " + fullPath + ". Do you want to erase it? [Y/N]: ")
-            if yesno.lower() is "y":
-                shutil.rmtree(fullPath)
-                os.makedirs(fullPath)
+
+        path = basePath + "/.egpg"
+        if os.path.exists(path):
+            yesno = input("There is an old directory " + path + ". Do you want to erase it? [Y/N]: ")
+            if yesno.lower() == "y":
+                shutil.rmtree(path)
             else:
                 exit(1)
-        else:
-            os.makedirs(fullPath)
 
+    os.makedirs(path)
+    innerPath = path + "/.gnupg"
+    os.makedirs(innerPath)
+    with open(innerPath + "/gpg-agent.conf", "w") as agentConf:
+        toWrite = """
+quiet
+pinentry-program /usr/bin/pinentry-tty
+allow-loopback-pinentry
+default-cache-ttl 300
+max-cache-ttl 999999
+"""
+        agentConf.write(toWrite)
+
+    with open(innerPath + "/gpg.conf", "w") as gpgConf:
+        toWrite = """
+keyid-format long
+default-cert-expire 1y
+"""
+        gpgConf.write(toWrite)
+
+    with open(path + "/config.sh", "w") as configFile:
+        toWrite = """
+# If true, push local changes to the keyserver network.
+# Leave it empty (or comment it out) to disable.
+SHARE=
+#KEYSERVER=hkp://keys.gnupg.net
+
+# GPG homedir. If "default", then the default one will be used,
+# (whatever is in the environment $GNUPGHOME, usually ~/.gnupg).
+GNUPGHOME="$(realpath "$EGPG_DIR")/.gnupg"
+
+# Path of the dongle.
+DONGLE=
+
+# If true, print debug output.
+DEBUG=
+"""
+        configFile.write(toWrite)
+
+    env_setup(path, (os.environ.get("HOME") + "/.bashrc"))
+
+def env_setup(path, rcFile):
+    with open(rcFile, "a") as rc:
+        toWrite = """
+### start pgpg config
+export GPG_TTY=$(tty)
+export EGPG_DIR={}
+#export GNUPGHOME={}/.gnupg
+### end pgpg config
+""".format(path, path)
+
+        rc.write(toWrite)
+        print("Appended the following lines to your .bashrc:")
+        print(toWrite)
+        print("Please reload it to enable the new config:\n\tsource ~/.bashrc")
 
 def main():
     if len(sys.argv) == 0:
         cmd_init_help()
-    print(str(sys.argv))
     if len(sys.argv) > 2:
         cmd_init(sys.argv[2])
     else:
