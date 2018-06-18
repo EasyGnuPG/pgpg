53f7177d7dfd44673e4346c5a8df223b9566fe55
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-06-18 06:43:26 +0530

    Add keyserver functionality
    display an error if no matching key is found

--- src/gpg/seal.py
+++ src/gpg/seal.py
@@ -2,6 +2,18 @@ import gpg
 import os
 import sys
 
+def in_keyring(recipient):
+    """
+    Check if a valid key is present in the keyring matching
+    recipient
+    """
+    matched_keys=list(gpg.Context().keylist(recipient))
+    valid_keys=list(filter(lambda k:k.revoked==0 and k.expired==0, matched_keys))
+    if(len(valid_keys)==0):
+        return False
+    else:
+        return True
+
 
 def get_selected_key(keys):
     """
@@ -9,26 +21,25 @@ def get_selected_key(keys):
     Currently displaying all at once; no N)ext functionality
     """
 
-    keys=list(keys)
-    if len(keys)==0:
-        return None
+    all_keys=list(keys)
+    valid_keys=list(filter(lambda k:k.revoked==False and k.expired==False, all_keys))
 
-    if len(keys)==1:
-        return keys
+    if len(valid_keys)==1:
+        return valid_keys
 
-    for sno, key in enumerate(keys):
+    for sno, key in enumerate(valid_keys):
         print(sno+1, key.uids[0].uid, key.fpr)
     
     chosen_keys=[]
     while(True):
         try:
-            print("Enter the number(s) to select a key, Q)uit")
+            print("Enter the number(s) to select a key, Q)uit", end='>')
             indices = input().strip().split()
             for index in indices:
                 if(index.lower()=='q'):
                     return None
                 else:
-                    chosen_keys.append(keys[int(index)-1])
+                    chosen_keys.append(valid_keys[int(index)-1])
             
             return chosen_keys
 
@@ -50,6 +61,11 @@ def seal(file_path, recipients):
             keys = c.keylist(pattern=recipient)
             chosen_keys = get_selected_key(list(keys))
             seal_list.extend(chosen_keys)
+        
+        # print the list
+        print("The following recipients were selected:")
+        for sno,key in enumerate(seal_list):
+            print(sno+1, key.uids[0].uid, key.fpr)       
 
         # encrypt
         seal_path=file_path+".sealed"
@@ -59,11 +75,13 @@ def seal(file_path, recipients):
                     _cyphertext, _result, _sign_result  = c.encrypt(infile,
                         recipients=seal_list, sign=True, sink=outfile)
                 except gpg.errors.InvalidRecipients:
-                    print("Some of the recipients are not trusted. Do you want to proceed anyway[y/N]")
+                    print("Some of the recipients are not trusted."
+                    " Do you want to proceed anyway[y/N]", end='>')
                     answer=input().lower()
                     if(answer=='y' or answer=='yes'):
                         _cyphertext, _result, _sign_result  = c.encrypt(infile,
-                        recipients=seal_list, sign=True, sink=outfile, always_trust=True)
+                        recipients=seal_list, sign=True,
+                        sink=outfile, always_trust=True)
                     else:
                         exit(3)
                     
@@ -78,6 +96,13 @@ def seal(file_path, recipients):
 if __name__ == "__main__":
     file_path = sys.argv[1]
     recipients = sys.argv[2:]
-    print(file_path)
-    print(recipients)
+    
+    for recipient in recipients:
+        if(not in_keyring(recipient)):
+            sys.stderr.write("Sorry! No matching contact for `{recipient}`\n"
+                    "Please add the person to your contacts first!\n"
+                    "For help on adding contacts run `egpg contact help`\n\n"
+                    .format(recipient=recipient))
+            exit(1)
+    
     seal(file_path, recipients)
