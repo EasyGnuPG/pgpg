ea27ee776f696ae0120dffe034f76e33c43171f8
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-06-23 22:41:13 +0530

    update for pep8

--- src/gpg/open.py
+++ src/gpg/open.py
@@ -4,8 +4,9 @@ import sys
 import time
 import textwrap
 
+
 def print_signatures(verify_result):
-    c=gpg.Context()
+    c = gpg.Context()
     for signature in verify_result.signatures:
         user = c.get_key(signature.fpr).uids[0].uid
         fpr = signature.fpr
@@ -16,10 +17,11 @@ def print_signatures(verify_result):
                     with key {fingerprint}
                     made at {time}
                     '''.format(user=user,
-                                fingerprint=fpr,
-                                time=signed_time)
+                               fingerprint=fpr,
+                               time=signed_time)
 
         print(textwrap.dedent(message))
+
 
 def open_file(sealed_file_path, output_file_path):
     """
@@ -30,39 +32,40 @@ def open_file(sealed_file_path, output_file_path):
         c = gpg.Context()
         with open(sealed_file_path, "rb") as cfile:
             try:
-                plaintext, result, verify_result = c.decrypt(cfile,verify=True)
-                
-                if(os.environ['DEBUG']=='yes'):
+                plaintext, result, verify_result = c.decrypt(
+                    cfile, verify=True)
+
+                if os.environ['DEBUG'] == 'yes':
                     print(verify_result, result)
-                
+
                 print_signatures(verify_result)
 
             except gpg.errors.BadSignatures:
                 print("Could not verify signatures")
                 exit(3)
-        
-        
+
         while(os.path.exists(output_file_path)):
-            print("File {output} already exists".format(output=output_file_path))
+            print("File {output} already exists"
+                  .format(output=output_file_path))
             print("Do you want to overwrite?(y/N)", end=">")
-            
-            ans=input()
-            if(ans.lower()=='y'):
+
+            ans = input()
+            if ans.lower() == 'y':
                 break
             else:
-                print("New file name",end=":")
-                output_file_path=input()
-            
+                print("New file name", end=":")
+                output_file_path = input()
+
         with open(output_file_path, "wb") as nfile:
             nfile.write(plaintext)
 
     except BaseException:
-        if(os.environ['DEBUG']=='yes'):
+        if os.environ['DEBUG'] == 'yes':
             raise
         else:
             exit(2)
 
-    
+
 if __name__ == "__main__":
     sealed_file_path = sys.argv[1]
     output_file_path = sys.argv[2]
