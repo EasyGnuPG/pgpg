7fe888f167983b06ee2a67652c377f7a0349691b
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-07-02 14:51:10 +0530

    export contact added

---
+++ src/gpg/contact/export.py
@@ -0,0 +1,38 @@
+import gpg
+import sys
+import os
+
+
+def export(export_path, contacts):
+    c = gpg.Context(armor=True)
+    if(export_path == "-"):
+        export_file = sys.stdout
+    else:
+        export_file = open(export_path, "w")
+
+    try:
+        for user in contacts:
+            expkey = gpg.Data()
+            c.op_export(user, 0, expkey)
+            expkey.seek(0, os.SEEK_SET)
+            expstring = expkey.read()
+            if expstring:
+                export_file.write(expstring.decode())
+            else:
+                sys.stderr.write("No keys found for %s \n" % user)
+                exit(1)
+
+    except BaseException:
+        if os.environ['DEBUG'] == 'yes':
+            raise
+        exit(2)
+
+    if(export_path != "-"):
+        export_file.close()
+
+
+if __name__ == "__main__":
+    export_path = sys.argv[1]
+    contacts = sys.argv[2:]
+    print(contacts)
+    export(export_path, contacts)
