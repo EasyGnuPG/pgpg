9dc4c9cce1381e8db84bb8e8194c88e965569bed
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-08-07 14:47:18 +0530

    add receive

---
+++ src/gpg/contact/receive.py
@@ -0,0 +1,27 @@
+import sys
+import gpg
+from fn.auxiliary import handle_exception, fail, print_debug
+from fn.hkp import Server
+
+
+@handle_exception()
+def receive(serverurl, keystring):
+    server = Server(serverurl)
+    # TODO: check keyid pattern vailidity
+    fullKey = server.get(keystring)
+
+    if(fullKey in [None, []]):
+        fail("No keys found")
+
+    c = gpg.Context()
+    c.op_import(fullKey.encode())
+    result = c.op_import_result()
+    if result is None:
+        fail("Could not import contact")
+        print_debug(result)
+
+
+if __name__ == "__main__":
+    serverurl = sys.argv[1]
+    keystring = sys.argv[2]
+    receive(serverurl, keystring)
